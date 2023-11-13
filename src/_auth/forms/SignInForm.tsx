9f80e6a4a74025f9f09c775ad5f895a7439a1cad
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-11-13 18:47:08 +0200

    feat:created db collections, finished auth logic

--- src/_auth/forms/SignInForm.tsx
+++ src/_auth/forms/SignInForm.tsx
@@ -1,63 +1,141 @@
-
-
+import { Link, useNavigate } from "react-router-dom";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { SigninvalidationShema } from '@/lib/validation/index';
+import { SigninvalidationShema } from "@/lib/validation";
+import { useToast } from "@/components/ui/use-toast";
 
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-
+import Loader from "@/components/shared/Loader";
+import {
+  useSignInAccount,
+} from "@/lib/react-query/queriesAndMutations";
+import { useUSerContext } from "@/context/AuthContext";
 
 const SignInForm = () => {
- 
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { checkAuthUser, isLoading: isUserLoading } = useUSerContext();
+
+  const { mutateAsync: signInAccount, isPending } =
+    useSignInAccount();
 
   const form = useForm<z.infer<typeof SigninvalidationShema>>({
     resolver: zodResolver(SigninvalidationShema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
     },
   });
 
- 
-  function onSubmit(values: z.infer<typeof SigninvalidationShema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SigninvalidationShema>) {
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (!session) {
+      return toast({
+        title: "Sign up failed. Please try again.",
+      });
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+      navigate("/");
+    } else {
+      return toast({
+        title: "Sign up failed. Please try again.",
+      });
+    }
   }
+
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Log in to your account!
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2"></p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
           <FormField
             control={form.control}
-            name="username"
+            name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input
+                    type="email"
+                    className="shad-input"
+                    placeholder="Email"
+                    {...field}
+                  />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    className="shad-input"
+                    placeholder="Password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={isUserLoading}
+            type="submit"
+            className="shad-button_primary"
+          >
+            {isUserLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Sign in"
+            )}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Don't have a account?{" "}
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Sign up
+            </Link>
+          </p>
         </form>
-      </Form>
-    </div>
+      </div>
+    </Form>
   );
-}
- 
+};
+
 export default SignInForm;
